bb05e4030dc3a794323be951254a7d8342b77320
Gen1xLol <gen1x.01.cool@gmail.com>
2024-12-09 16:27:16 -0300

    Create epic-ultra-gamer-code.js

---
+++ epic-ultra-gamer-code.js
@@ -0,0 +1,6 @@
+alert("SURPRISE!")
+alert("This code was requested by the Prism Extension in PenguinMod. It didn't ask you before doing that")
+alert("So... here I am! My only purpose as a file in this GitHub repo is to alert Jeremy and the other devs about this vulnerability.")
+alert("Pretty sad purpose, if I do say so myself...")
+alert("Anyway, this could've been used to do some pretty sketchy stuff. Imagine if I was a script that could show you gore or something.")
+alert("G'bye!")
